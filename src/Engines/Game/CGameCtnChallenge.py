7c2c6ef9d2c329e8d7f60b7c82d2a1ac3110ebce
JailMan <jailmanguy1@gmail.com>
2025-01-05 21:43:21 +0300

    Update CGameCtnChallenge.py

--- src/Engines/Game/CGameCtnChallenge.py
+++ src/Engines/Game/CGameCtnChallenge.py
@@ -1,4 +1,5 @@
 from GBEggs import GbxReader, GbxException
+import io
 
 class CGameCtnChallenge:
     def __init__(self, Reader: GbxReader):
@@ -10,10 +11,41 @@ class CGameCtnChallenge:
             ThumbnailChunk = self.Reader.GetHeaderChunkById(0x3043007)
             if ThumbnailChunk != None:
                 self.Thumbnail = ThumbnailChunk.Data
+            
+            AuthorInfoChunk = self.Reader.GetHeaderChunkById(0x3043008)
+            if AuthorInfoChunk != None:
+                self.MapInfoVersion = AuthorInfoChunk.ReadUint32()
+                self.AuthorVersion = AuthorInfoChunk.ReadUint32()
+                self.AuthorLogin = AuthorInfoChunk.ReadString()
+                self.AuthorNickname = AuthorInfoChunk.ReadString()
+                self.AuthorZone = AuthorInfoChunk.ReadString()
+                self.AuthorExtraInfo = AuthorInfoChunk.ReadString()
+            
+            XmlChunk = self.Reader.GetHeaderChunkById(0x03043005)
+            if XmlChunk != None:
+                self.Xml = XmlChunk.ReadString()
+
+    def SaveChanges(self):
+        ThumbnailChunk = self.Reader.GetHeaderChunkById(0x3043007)
+        if ThumbnailChunk != None:
+            ThumbnailChunk.SetData(self.Thumbnail)
+            self.Reader.HeaderChunks[self.Reader.GetHeaderChunkIdxById(0x3043007)] = ThumbnailChunk
+
+        AuthorInfoChunk = self.Reader.GetHeaderChunkById(0x3043008)
+
+        if AuthorInfoChunk != None:
+            AuthorInfoChunk.WriteUint32(self.MapInfoVersion)
+            AuthorInfoChunk.WriteUint32(self.AuthorVersion)
+            AuthorInfoChunk.WriteString(self.AuthorLogin)
+            AuthorInfoChunk.WriteString(self.AuthorNickname)
+            AuthorInfoChunk.WriteString(self.AuthorZone)
+            AuthorInfoChunk.WriteString(self.AuthorExtraInfo)
+
+        AuthorInfoChunk.SetDataFromWriteBuffer()
+        self.Reader.HeaderChunks[self.Reader.GetHeaderChunkIdxById(0x3043008)] = AuthorInfoChunk
 
     @property
     def ThumbnailPillow(self):
-        import io
         from PIL import Image, ImageOps
         BytesIo = io.BytesIO(self.Thumbnail)
         Version = int.from_bytes(BytesIo.read(4))
